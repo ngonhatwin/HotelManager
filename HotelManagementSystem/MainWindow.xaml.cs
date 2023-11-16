bd367fbd73f7e503a2c0e86d670e442004729566
ngonhatwin <ngonhatwin@gmail.com>
2023-11-16 19:16:48 +0700

    add new project

---
+++ HotelManagementSystem/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Show_Room_Click(object sender, RoutedEventArgs e)
+        {
+            Rooms myroom = new Rooms();
+            myroom.Show();
+            this.Hide();
+        }
+
+        private void Show_Student_Click(object sender, RoutedEventArgs e)
+        {
+            Student mystudent = new Student();
+            mystudent.Show();
+            this.Hide();
+        }
+
+        private void Show_Emloyee_Click(object sender, RoutedEventArgs e)
+        {
+            Emloyee myemloyee = new Emloyee();
+            myemloyee.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ HotelManagementSystem/Rooms.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for Rooms.xaml
+    /// </summary>
+    public partial class Rooms : Window
+    {
+        public Rooms()
+        {
+            InitializeComponent();
+        }
+        private void RoomNumberTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Xóa Placeholder khi TextBox nhận focus
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Room Number")
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void RoomNumberTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Khôi phục lại Placeholder nếu TextBox trống
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Room Number";
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
